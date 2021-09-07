645500901b996fbf626ed6e7cbbefbe8c34e2610
Alexander <aleksandr.chasovnikov@gmail.com>
2021-09-07 12:27:27 +0300

    Rename files, add english, add folders
    Rename files, add english, add folders

---
+++ JavaScript/conspectus/10_Classes.js
@@ -0,0 +1,335 @@
+
+/**
+ * В JavaScript класс – это разновидность функции.
+ * 
+ * Конструкция class User {...}:
+ *  1.Создаёт функцию User с кодом из метода constructor.
+ *  2.Сохраняет все методы в User.prototype.
+ * 
+ * Конструктор класса не может быть вызван без new.
+ * Методы класса изначально являются неперечислимыми (флаг enumerable в false).
+ * Классы всегда используют use strict.
+ * Как и функции, классы можно определять внутри другого выражения, 
+ *      передавать, возвращать, присваивать и т.д.
+ */
+ class User {
+    constructor(name) { this.name = name; }
+    sayHi() { alert(this.name); }
+}
+
+// класс - это функция
+alert(typeof User); // function
+
+// ...или, если точнее, это метод constructor
+alert(User === User.prototype.constructor); // true
+
+// Методы находятся в User.prototype, например:
+alert(User.prototype.sayHi); // alert(this.name);
+
+// в прототипе ровно 2 метода
+alert(Object.getOwnPropertyNames(User.prototype)); // constructor, sayHi
+
+
+/**
+ * Аналогично Named Function Expression, Class Expression может иметь имя.
+ * Если у Class Expression есть имя, то оно видно только внутри класса:
+ */
+let User = class MyClass {
+    sayHi() {
+      alert(MyClass); // имя MyClass видно только внутри класса
+    }
+};
+new User().sayHi(); // работает, выводит определение MyClass
+alert(MyClass); // ошибка, имя MyClass не видно за пределами класса
+
+
+
+/**
+ * После extends разрешены любые выражения.
+ * Пример вызова функции, которая генерирует родительский класс:
+ */
+
+function f(phrase) {
+    return class {
+      sayHi() { alert(phrase) }
+    }
+}
+class User extends f("Привет") {}  
+new User().sayHi(); // Привет
+
+
+/**
+ * super.method(...) вызывает родительский метод.
+ * super(...) вызывает родительский конструктор (работает только 
+ *      внутри нашего конструктора).
+ */
+class Rabbit extends Animal {
+    constructor(name) {
+        super();    
+        this.name = name;
+    }
+
+    hide() {
+        alert(`${this.name} прячется!`);
+    }
+
+    stop() {
+        super.stop(); // вызываем родительский метод stop
+        this.hide(); // и затем hide
+    }
+}
+
+
+/** 
+ * У стрелочных функций нет super.
+ * При обращении к super стрелочной функции он берётся из внешней функции:
+ */
+class Rabbit extends Animal {
+    stop() {
+      setTimeout(() => super.stop(), 1000); // вызывает родительский stop после 1 секунды
+    }
+  }
+
+
+
+/**
+ * Если класс расширяет другой класс и не имеет конструктора, то 
+ *      автоматически создаётся такой «пустой» конструктор.
+ * В классах-потомках конструктор обязан вызывать super(...)
+ *      перед использованием this.
+ */
+class Rabbit extends Animal {
+    // генерируется для классов-потомков, у которых нет своего конструктора
+    constructor(...args) {
+        super(...args);
+    }
+}
+
+
+
+/**
+ * Статические свойства и методы.
+ * 
+ * Принадлежат классу «в целом», а не относятся к конкретному объекту класса.
+ * Обозначаются ключевым словом static.
+ * Значением this здесь является сам конструктор класса.
+ * Статические методы также используются в классах, относящихся к базам данных.
+ * Статические свойства и методы наследуются.
+ */
+class Article {
+    constructor(title, date) {
+        this.title = title;
+        this.date = date;
+      }
+    static publisher = "Илья Кантор";
+    
+    static compare(articleA, articleB) {
+        return articleA.date - articleB.date;
+    }
+}
+const articles = new Article('dvfvf', 0);
+articles.sort(Article.compare);
+alert( Article.publisher ); // Илья Кантор
+
+
+
+
+class Rabbit extends Object {}
+alert( Rabbit.prototype.__proto__ === Object.prototype ); // (1) true
+alert( Rabbit.__proto__ === Object ); // (2) true
+
+
+/**
+ * Инкапсуляция.
+ * 
+ * Св-ва и меоды м.б.:
+ *      1.Публичные.
+ *      2.Приватные - префикс # (реализовано на уровне языка).
+ *          Они доступны только внутри класса.
+ *          С приватными свойствами this['#name'] не работает.
+ *      3.Защищенные - префикс _ (НЕ реализовано на уровне языка).
+ *          Реализ-ся с помощью ф-ий и условий.
+ * У нас может быть два одинаковых поля одновременно – приватное и публичное.
+ * Чтобы создать св-во только для чтения, нужно сделать для него только геттер,
+ *      не делая сеттера.
+ */
+class CoffeeMachine {
+    _waterAmount = 0;
+
+    /**
+     * Использование функций get.../set... предпочтительнее, чем геттеров/сеттеров:
+     */
+    setWaterAmount(value) {
+        if (value < 0) throw new Error("Отрицательное количество воды");
+        this._waterAmount = value;
+    }
+    
+    getWaterAmount() {
+        return this._waterAmount;
+    }
+  
+    /**
+     * Использование функций get.../set... предпочтительнее, чем геттеров/сеттеров:
+     */
+    // set waterAmount(value) {
+    //   if (value < 0) throw new Error("Отрицательное количество воды");
+    //   this._waterAmount = value;
+    // }
+  
+    // get waterAmount() {
+    //   return this._waterAmount;
+    // }
+  
+    constructor(power) {
+      this._power = power;
+    }
+
+    get power() {
+        return this._power;
+    }
+  
+}
+
+// создаём новую кофеварку
+let coffeeMachine = new CoffeeMachine(100);
+// устанавливаем количество воды
+coffeeMachine.waterAmount = -10; // Error: Отрицательное количество воды
+coffeeMachine.power = 25; // Error (no setter)
+
+
+
+/**
+ * Расширение встроенных классов
+ * 
+ * От встроенных классов (Array, Map, ...) можно наследовать.
+ * Встроенные методы (filter, map, ...) возвращают новые объекты. 
+ *      Для этого они используют свойство объекта "constructor".
+ * Symbol.species возращает свой конструктор.
+ * Другие коллекции (Map, Set, ...) работают аналогично.
+ * Встроенные классы не наследуют статические методы друг друга.
+ */
+ class PowerArray extends Array {
+    // свой метод
+    isEmpty() {
+      return this.length === 0;
+    }
+  
+    // встроенные методы массива будут использовать этот метод как конструктор
+    static get [Symbol.species]() {
+      return Array;
+    }
+}
+
+let arr = new PowerArray(1, 2, 5, 10, 50);
+alert(arr.isEmpty()); // false
+
+// filter создаст новый массив, используя arr.constructor[Symbol.species] как конструктор
+let filteredArr = arr.filter(item => item >= 10);
+
+// filteredArr не является PowerArray, это Array
+alert(filteredArr.isEmpty()); // Error: filteredArr.isEmpty is not a function
+
+
+
+/**
+ * instanceof 
+ *      позволяет проверить принадлежность к классу с учётом наследования.
+ */
+let arr = [1, 2, 3];
+alert( arr instanceof Array ); // true
+alert( arr instanceof Object ); // true
+
+// Symbol.hasInstance - изменяет поведение "instanceof".
+class MyArray {
+    static [Symbol.hasInstance](instance) {
+      return Array.isArray(instance);
+    }
+}
+console.log([] instanceof MyArray); // true
+
+
+
+/**
+ * У toString имеются скрытые возможности:
+ *      можно использовать его как расширенную версию typeof 
+ *          и как альтернативу instanceof.
+ * 
+ */
+let s = Object.prototype.toString;
+
+alert( s.call(123) ); // [object Number]
+alert( s.call(null) ); // [object Null]
+alert( s.call(alert) ); // [object Function]
+
+/**
+ * Поведение toString можно настраивать через Symbol.toStringTag.
+ */
+let user = {
+[Symbol.toStringTag]: "User"
+};
+
+alert( {}.toString.call(user) ); // [object User],   typeof: [object]
+
+
+
+
+/**
+ * Примесь (trait) – это класс, методы которого предназначены для использования 
+ *      в других классах, причём без наследования от примеси.
+ * не используем примесь саму по себе, а используем её, 
+ *      чтобы добавить функциональность другим классам.
+ * Примеси могут наследовать друг друга.
+ */
+// примесь
+let sayHiMixin = {
+    sayHi() {
+      alert(`Привет, ${this.name}`);
+    },
+    sayBye() {
+      alert(`Пока, ${this.name}`);
+    }
+  };
+  
+  // использование:
+  class User {
+    constructor(name) {
+      this.name = name;
+    }
+  }
+  
+  // копируем методы
+  Object.assign(User.prototype, sayHiMixin);
+  
+  // теперь User может сказать Привет
+  new User("Вася").sayHi(); // Привет, 
+
+  let sayMixin = {
+    say(phrase) {
+      alert(phrase);
+    }
+  };
+  
+// наследование примесей:
+  let sayHiMixin = {
+    __proto__: sayMixin, // (или мы можем использовать Object.create для задания прототипа)
+  
+    sayHi() {
+      // вызываем метод родителя
+      super.say(`Привет, ${this.name}`); // (*)
+    },
+    sayBye() {
+      super.say(`Пока, ${this.name}`); // (*)
+    }
+  };
+  
+  class User {
+    constructor(name) {
+      this.name = name;
+    }
+  }
+  
+  // копируем методы
+  Object.assign(User.prototype, sayHiMixin);
+  
+  // теперь User может сказать Привет
+  new User("Вася").sayHi(); // Привет, Вася!
---
+++ JavaScript/conspectus/2_strings.js
@@ -0,0 +1,99 @@
+
+ /**============ Тип данных String =============
+ * Строки неизменяемы
+ * Строки в JavaScript кодируются в UTF-16
+ * 3 вида кавычек
+ * Можно перебрать строку посимвольно, используя for..of
+ */
+ let str = "Привет"; // Применяются в JSON
+ let str2 = 'Одинарные кавычки тоже подойдут';
+ let phrase = `Обратные кавычки позволяют встраивать переменные ${str} 
+     и могут занимать 
+     более одной строки`;     
+
+let name = "Ilya";
+alert( `hello ${name}` );   // hello Ilya
+alert( `hello ${1}` );      // hello 1
+alert( `hello ${"name"}` ); // hello name
+
+
+let str = `Hello`;
+
+// получаем первый символ
+alert( str[0] );            // H
+alert( str.charAt(0) );     // H
+
+alert( str[1000] );         // undefined    !!!
+alert( str.charAt(1000) );  // ''           !!!
+// получаем последний символ
+alert( str[str.length - 1] ); // o
+
+
+'Interface'.toUpperCase(); // INTERFACE
+'Interface'.toLowerCase(); // interface
+
+'Widget'.indexOf('id', 3); // -1, индекс подстроки, начиная поиск с позиции 3
+    ~'Widget'.indexOf('id', 3); // 0, то же, что -(-1+1)
+'Widget'.lastIndexOf('e', 3); //, поиск от конца к началу, начиная с индекса 3
+
+"Midget".includes("id", 3); // false, поиск подстроки с позиции 3
+"Widget".startsWith("Wid"); // true, поиск начала строки
+"Widget".endsWith("get"); // true, поиск конца строки
+
+let arr = 'Вася, Петя, Маша, Саша'.split(', ', 2); // [Вася, Петя]
+
+let str = "stringify";
+str.slice(3, 5); // "in", строка от 3 до 5
+str.substring(2, 6); // "ring"
+    str.substring(6, 2); // "ring"
+str.substr(3, 2); // "in", строка от 3 длинной 2
+/**
+ * Формально у метода substr есть небольшой недостаток: 
+ * он описан не в собственно спецификации JavaScript, 
+ * а в приложении к ней — Annex B
+ */
+
+
+// Вернуть код символа в UTF-16
+"z".codePointAt(0); // 122
+"Z".codePointAt(0); // 90
+// "Z" < "z"  (90 < 122) - сравнение идет по коду
+
+// Создаёт символ по его коду
+String.fromCodePoint(90); // Z
+// используя \u с шестнадцатеричным кодом символа:
+String.fromCodePoint('\u005a'); // Z
+
+/**
+ * Правильное сравнение.
+ * Сравнение в соответствии с ПРАВИЛАМИ языка (а не по коду):
+ * -1, если str < str2.
+ * 1, если str > str2.
+ * 0, если str === str2.
+ */
+ str.localeCompare(str2);
+'Österreich'.localeCompare('Zealand'); // -1
+
+/**
+ * String.fromCodePoint и str.codePointAt 
+ * правильно работают с СУРРОГАТНЫМИ ПАРАМИ в отличие от
+ * похожим методов String.fromCharCode и str.charCodeAt
+ */
+
+/**
+ * Диакритические знаки и нормализация
+ */
+// после S добавить специальный символ «точка сверху» (код \u0307)
+alert( 'S\u0307' ); // Ṡ
+alert( 'S\u0307\u0323' ); // Ṩ
+
+let s1 = 'S\u0307\u0323'; // Ṩ, S + точка сверху + точка снизу
+let s2 = 'S\u0323\u0307'; // Ṩ, S + точка снизу + точка сверху
+alert( `s1: ${s1}, s2: ${s2}` );
+alert( s1 == s2 ); // false, хотя на вид символы одинаковы (?!)
+/**
+ * Для решения этой проблемы есть алгоритм «юникодной нормализации», 
+ * приводящий каждую строку к единому «нормальному» виду.
+ * Его реализует метод str.normalize().
+ */
+ alert( "S\u0307\u0323".normalize() == "S\u0323\u0307".normalize() ); // true
