e7701d482256e16719b90b7907ddf971c60b6037
Alexander <aleksandr.chasovnikov@gmail.com>
2021-10-29 20:29:24 +0300

    Update DOM.js

--- JavaScript/conspectus/browser/DOM.js
+++ JavaScript/conspectus/browser/DOM.js
@@ -41,3 +41,56 @@ elem.childNodes[elem.childNodes.length - 1] === elem.lastChild;
 // Проверка наличия дочерних узлов
 elem.hasChildNodes();
 
+
+
+/// ВЫБОР ЭЛЕМЕНТОВ ДОКУМЕНТА
+// .querySelector() и .querySelectorAll() - поиск элем-ов в DOM по селекторам CSS:
+// div          - любой элемент <div>
+// #nav         - эл. с id="nav"
+// .warning     - эл-ы с class="warning"
+// p[lang="fr"] - <p lang="fr">
+// *[name="x"]  - эл-ы с name="x"
+// #long span   - любой потомок <span> элемента c id="long"
+// #long>span   - любой дочерний <span> элемента c id="long"
+// body>h1:first-child - первый дочерний <h1> эл-та <body>
+// img + p.caption     - эл. <p class="caption">, идущий сразу после <img>
+// h2 ~ p       - <p> после <h2> и явл-ся родственным ему
+// button, input[type="button"]     // Все <button> и <input type="button">
+let spinner = document.querySelector('#spinner');       // возр. первый совпад.эл-т или null
+let titles = document.querySelectorAll('h1, h2, h3');   // возр. итерир. объект NodeList
+// В querySelectorAll() NodeList - "пассивный" (длина и содержимое не может изменяться)
+
+// querySelector() и querySelectorAll() реализ. для классов Document и Element:
+let elem = spinner.querySelector('#elem');              // возр. только потомки эл-та
+
+// closest() - метод в классе Element. В отличие от querySelector() ищет выше по дереву
+// полезен, когда вы регистр. обр-к соб. на высок. уровне в дереве документа
+
+// Найти ближайший объемл-й <a>, кот-й имеет href
+let hyperlink = event.target.closest('a[href]');
+
+// вернуть true, если эл-т е нах-ся внутри спискового HTML-элемента
+function insideList(e) {
+    return e.closest('ul,ol,dl') !== null;
+}
+
+// Метод matches()
+// вернуть true, если е - заголовочный HTML-элемент
+function isHeading(e) {
+    return e.matches('h1,h2,h3,h4,h5,h6');
+}
+
+/// СТАРЫЕ МЕТОДЫ ВЫБОРА ЭЛЕМЕНТОВ
+// Возр. "активный"(длина и содержимое может меняться) NodeList (кроме getElementById())
+// подобен      document.querySelector('#sect1')
+let sect1 = document.getElementById('sect1');
+
+// подобен      document.querySelectorAll('*[name="color"]')
+let colors = document.getElementsByName('color');
+
+// подобен      document.querySelectorAll('h1')
+let heading = document.getElementsByTagName('h1');
+
+// подобен      document.querySelectorAll('.tooltip')
+let tooltips = document.getElementsByClassName('tooltip');
+
