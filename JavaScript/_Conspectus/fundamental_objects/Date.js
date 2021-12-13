f61dc6eb76193ddc5a4be22fc85e041b3fdd8830
Alexander <aleksandr.chasovnikov@gmail.com>
2021-12-13 16:12:31 +0300

    add folder _Conspectus

--- JavaScript/_Conspectus/fundamental_objects/Date.js
+++ JavaScript/_Conspectus/fundamental_objects/Date.js
@@ -293,4 +293,43 @@ function JSClock() {
     temp += ((second < 10) ? ":0" : ":") + second;
     temp += (hour >= 12) ? " P.M." : " A.M.";
     return temp;
-  }
+}
+
+
+// Секундомер в консоли
+class Clock {
+    timer = null;
+
+    constructor(templateObj) {
+        this.template = templateObj.template;
+    }
+
+    render() {
+        let date = new Date();
+
+        let hours = date.getHours();
+        if (hours < 10) hours = '0' + hours;
+
+        let mins = date.getMinutes();
+        if (mins < 10) mins = '0' + mins;
+
+        let secs = date.getSeconds();
+        if (secs < 10) secs = '0' + secs;
+
+        let output = this.template.replace('h', hours).replace('m', mins).replace('s', secs);
+
+        console.log(output);
+    }
+
+    stop() {
+        clearInterval(this.timer);
+    }
+
+    start() {
+        this.render();
+        this.timer = setInterval(this.render.bind(this), 1000);
+    }
+}
+
+let clock = new Clock({ template: 'h:m:s' });
+clock.start();
