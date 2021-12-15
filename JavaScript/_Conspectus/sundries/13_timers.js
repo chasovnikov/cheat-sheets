d1a6baf3559b47b28bfcf3b433ad1c92c991e1f4
Alexander <aleksandr.chasovnikov@gmail.com>
2021-12-15 07:02:38 +0300

    Update Async_programming and other

--- JavaScript/_Conspectus/sundries/13_timers.js
+++ JavaScript/_Conspectus/sundries/13_timers.js
@@ -1,19 +1,18 @@
-
 // крутим цикл для задержки времени (блокирующая задержка)
 const sleep = msec => {
     const end = new Date().getTime() + msec;
     while (new Date().getTime() < end);
 };
 
-
 // на промисах
-const sleep = msec => new Promise(resolve => {
-    setTimeout(resolve, msec);
-});
+const sleep = msec =>
+    new Promise(resolve => {
+        setTimeout(resolve, msec);
+    });
 
 (async () => {
     console.log('Start sleep: ' + new Date().toISOString());
-    await sleep(3000);      // блокирования не происходит
+    await sleep(3000); // блокирования не происходит
     console.log('After sleep: ' + new Date().toISOString());
 })();
 
@@ -22,3 +21,25 @@ console.log('Start sleep: ' + new Date().toISOString());
 sleep(3000).then(() => {
     console.log('After sleep: ' + new Date().toISOString());
 });
+
+// ------------- timeout
+// Ф-ия обёртка для создания возможности timeout
+function timeout(msec, fn) {
+    let timer = setTimeout(() => {
+        if (timer) console.log('Function timeout');
+        timer = null;
+    }, msec);
+    return (...args) => {
+        if (timer) {
+            timer = nullfn(...args);
+        }
+    };
+}
+// Применение
+const fn = par => console.log('Function called, par:' + par);
+const fn100 = timeout(100, fn);
+const fn200 = timeout(200, fn);
+setTimeout(() => {
+    fn100('first'); // не выполнится
+    fn100('first'); // выполнится
+}, 150);
--- JavaScript/_Conspectus_Shemsedinov/Async_programming.js
+++ JavaScript/_Conspectus_Shemsedinov/Async_programming.js
@@ -108,7 +108,7 @@ async function f() {
 
 f().then(console.log('...')).catch(console.error('...'));
 
-// Functor + Chaining + composition
+// --------------- Functor + Chaining + composition
 const c1 = chain().do(f1, a1).do(f1, a1).do(f3, a3);
 c1();
 
@@ -142,3 +142,172 @@ function chain(prev = null) {
     };
     return cur;
 }
+
+// ------------------ Проблемы асинхронности
+// У промисов нет timeout (нужен, чтобы, если не успели за определен. время, то - прервать опер-ию)
+
+// Ф-ия обёртка для создания возможности timeout
+function timeout(msec, fn) {
+    let timer = setTimeout(() => {
+        if (timer) console.log('Function timeout');
+        timer = null;
+    }, msec);
+    return (...args) => {
+        if (timer) {
+            timer = nullfn(...args);
+        }
+    };
+}
+// Применение
+const fn = par => console.log('Function called, par:' + par);
+const fn100 = timeout(100, fn);
+const fn200 = timeout(200, fn);
+setTimeout(() => {
+    fn100('first'); // не выполнится
+    fn100('first'); // выполнится
+}, 150);
+
+// Промис нельзя отменить
+// Ф-ия обёртка для создания возможности отмены
+const cancelable = fn => {
+    const wrapper = (...args) => {
+        if (fn) return fn(...args);
+    };
+    wrapper.cancel = () => (fn = null);
+    return wrapper;
+};
+// Применение
+const fn = par => console.log('Function called, par:' + par);
+const f = cancelable(fn);
+f('first');
+f.cancel();
+f('second');
+
+// Разные полезные обёртки
+const f1 = timeout(1000, fn);
+const f2 = cancelable(fn);
+const f3 = once(fn); // вызывается не более одного раза
+const f4 = limit(10, fn); // вызывается не более 10 раз
+const f5 = throttle(10, 1000, fn); // вызывается не более 10 раз за 1 сек
+const f6 = debounce(1000, fn); // вызывается не более одного раза за 1 сек
+const f7 = utils(fn).limit(10).throttle(10, 1000).timeout(1000); // объединение ф-ий
+
+// Адаптеры асинхронности (Promisify and Callbackify)
+// В Node.js встроены в модуле utils
+// асинх. ф-ию оборачивает в промис
+const promise = promisify(asyncFunction);
+promise.then(/*...*/).catch(/*...*/);
+
+// Из промиса делает колбэк
+const callback = callbackify(promise);
+callbackify((err, value) => {
+    /*...*/
+});
+
+// --------------- Синхронный контракт привести к асинхронному
+//      (что была совместимость контр-ов и можно было использовать в цепочках)
+const last = arr => arr[arr.length - 1];
+const toAsync =
+    fn =>
+    (...args) => {
+        const callback = last(args);
+        args.pop;
+        callback(null, fn(...args));
+    };
+
+// -------------- Sync function to Promise
+const toPromise =
+    fn =>
+    (...args) =>
+        new Promise(resolve => resolve(fn(...args)));
+// Применение:
+const f1 = par => par;
+const f2 = par => par;
+const f3 = par => par;
+const f4 = par => par;
+console.log(f4(f3(f2(f1('value')))));
+const pf1 = toPromise(f1);
+const pf2 = toPromise(f2);
+const pf3 = toPromise(f3);
+const pf4 = toPromise(f4);
+Promise.resolve()
+    .then(pf1.bind(null, 'value'))
+    .then(pf2())
+    .then(pf3())
+    .then(pf4())
+    .then(data => console.log(data));
+
+// --------------- Конверторы (есть в библ-ке metasync)
+// err-back to Promise
+// Promise to err-back
+// sync function to Promise
+// sync function to err-back
+// Events to Promise
+// Promise to Events
+// Events to err-back
+// err-back to Events
+
+// ---------------- Библиотека Metasync
+
+// -------------- Последовательное и параллельное исполнение
+const f1 = sequential(
+    (name, cb) => fs.readFile(name, cb),
+    (data, cb) => http.get(data.url, cb),
+    (query, cb) => db.select(query, cb)
+);
+const f2 = parallel(dbReq1, dbReq2, dbReq3);
+
+// -------------- Параллельно-последовательная композиция
+const fx = metasync.flow([f1, f2, f3, [[f4, f5, [f6, f7], f8]], f9]);
+// [...]    - последоват.
+// [..[..[..]..]..] - параллельн.
+
+// ----------- Абстракция Data Collector (очень похожа на EventEmitter)
+const dc1 = new metasync.DataCollector(4); // 4 кусочка данных
+const dc2 = new metasync.DataCollector(4, 5000); // 5000 - время в мс
+dc1.on('error', (err, key) => {});
+dc2.on('timeout', (err, data) => {});
+dc2.on('done', (errs, data) => {});
+dc1.collector(data); // кусочки данных добавляются в коллекцию (после сбора всех --> "done")
+
+// Key Collector
+const keyCollector = new keyCollector(['k1', 'k2'], data => console.log(data));
+keyCollector.collector('k1', {});
+fs.readFile('HISTORY.md', (err, data) => keyCollector.collect('history', data));
+
+// Это будет быстрее, чем на колбэках, потому что на колбэках заполн-ся стэк,
+//      а через события заполнение стэка меньше
+
+const dc1 = metasync
+    .collect(3)
+    .timeout(5000)
+    .done((err, data) => {});
+dc(item);
+const dc2 = metasync
+    .collect(['key1', 'key2', 'key3'])
+    .timeout(5000)
+    .done((err, data) => {});
+dc2(key, value);
+
+// Collector feature
+const dc = metasync
+    .collect(count)
+    .distinct()
+    .done((err, data) => {});
+dc(key, error, vallue);
+dc.pick(key, value); // передает ключ и значение
+dc.fail(key, error); // ключу сопоставляет ошибку
+dc.take(key, fs.readFile, filename); // взамен: fs.readFile(filename, dc.bind(null, key));
+
+// ------------------ Queue (асинхронная очередь)
+// Очереди позволяют задать ограничение пропускной способности, чтобы не повисло приложение
+const cq = metasync
+    .queue(3) // 3 канала системы массового обслуживания (СМО)
+    .wait(2000)
+    .timeout(5000)
+    .throttle(100, 1000)
+    .process((item, cb) => cb(err, result)) // обработка запроса
+    .success(item => {})
+    .failure(item => {})
+    .done(() => {}) // результат или ошибка
+    .drain(() => {}); // Это событие случается, когда вся очередь рассосалась
